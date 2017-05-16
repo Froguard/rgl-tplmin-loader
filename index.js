e66189bfede17a1718b31e77f7dffc5dd4d84102
hzwangfei3 <hzwangfei3@corp.netease.com>
2017-05-16 23:54:40 +0800

    init

--- index.js
+++ index.js
@@ -1,49 +1,25 @@
-'use strict';
-let loaderUtils = require('loader-loaderUtils');
-// let assign = require('object-assign');
-// let compress = require('html-minifier').minify;
-
-const cleanRedundantCode = str => {
+function cleanRedundantCode(str){
     if(typeof str === 'string'){
+        // str = str.replace(/(<!--)([\s\S])*.*(-->)/g, ''); // html-comments  <!-- xxx -->
         str = str.replace(/{!([\s\S])*.*!}/g, '');    // regular-comments  {! xxx !}
         str = str.replace(/[\r\n]|\s+(?=[<{])/g, ''); // \r\n space before < & {
-        str = str.replace(/[}>]\s+/g, value => value.substr(0, 1)); // \r\n space after > & }
+         // \r\n space after > & }
+        try{
+            str = str.replace(/[}>]\s+/g, function(value){
+				return typeof(value)==='string' ? value.substr(0, 1) : '';
+			});
+        }catch(e){
+            console.log('出错', e);
+        }
     }
     return str;
 };
 
 module.exports = function(content) {
     this.cacheable && this.cacheable();
-    let opts = loaderUtils.parseQuery(this.query) || {};// loaderUtils.getOptions(this) || {};
-    opts.filename = this.resource;
-
     // remove redundant code
-    content = cleanRedundantCode(JSON.stringify(content));
-
+    content = JSON.stringify(cleanRedundantCode(String(content)));
     this.value = content;
-    return `module.exports = ${content}`;
-
-    // compress code
-    // opts = assign({
-    //     collapseWhitespace: true,
-    //     removeComments: true,
-    //     minifyJS: false,
-    //     minifyCSS: true,
-    //     ignoreCustomComments: [
-    //         /({!)(([\s\S])*.*)(!})/    // 忽略 regularJs自身的注释语法
-    //     ],
-    //     ignoreCustomFragments: [
-    //         /<(\/)?([\s\S])*.*(\/)?>/  // 忽略的自定义标签语法 <abc>,</abc>
-    //     ],
-    //     customEventAttributes: [
-    //         /^on[a-z]{3,}$/,
-    //         /^on-[a-z]{3,}$/,
-    //         /^r[a-z]{3,}$/,
-    //         /^r-[a-z]{3,}$/
-    //     ],
-    //     customAttrCollapse: [],
-    //     customAttrAssign: []
-    // }, opts);
-    // content = compress(content), opts);
+    return "module.exports = " + content;
 };
 // module.exports.raw = true;
