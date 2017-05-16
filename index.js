2c6894ba691bc9213b4fadcd19daa3b17d385369
froguardoge <figure_wf@163.com>
2017-05-17 02:10:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
+module.exports = function(content) {
+    this.cacheable && this.cacheable();
+    var tpl = content || '';
+    tpl = cleanRedundantCode(tpl);// remove redundant code
+    this.value = tpl;
+    return "module.exports = " + JSON.stringify(tpl);
+};
 function cleanRedundantCode(str){
     if(typeof str === 'string'){
-        // str = str.replace(/(<!--)([\s\S])*.*(-->)/g, ''); // html-comments  <!-- xxx -->
-        str = str.replace(/{!([\s\S])*.*!}/g, '');    // regular-comments  {! xxx !}
-        str = str.replace(/[\r\n]|\s+(?=[<{])/g, ''); // \r\n space before < & {
-         // \r\n space after > & }
-        try{
-            str = str.replace(/[}>]\s+/g, function(value){
-				return typeof(value)==='string' ? value.substr(0, 1) : '';
-			});
-        }catch(e){
-            console.log('出错', e);
-        }
+        str = str.trim();                              // left & right space
+        str = str.replace(/<!-[\s\S]*?-->/g, '');      // html-comments  <!-- xxx -->
+        str = str.replace(/{![\s\S]*?!}/g, '');        // regular-comments  {! xxx !}
+        str = str.replace(/\r|\n|\s+(?=[<{])/g, '');   // \r\n space before < & {
+        str = str.replace(/[}>]\s+/g, function(value){ // \r\n space after > & }
+            return typeof(value)==='string' ? value.substr(0, 1) : '';
+        });
     }
     return str;
 };
-
-module.exports = function(content) {
-    this.cacheable && this.cacheable();
-    // remove redundant code
-    content = JSON.stringify(cleanRedundantCode(String(content)));
-    this.value = content;
-    return "module.exports = " + content;
-};
-// module.exports.raw = true;
