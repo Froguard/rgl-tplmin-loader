455d8f8428df18496b8fade2a4d4cacea6fb0b01
hzwangfei3 <hzwangfei3@corp.netease.com>
2017-05-22 20:52:39 +0800

    add query

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+var loaderUtils = require("loader-utils");
+
 module.exports = function(content) {
     this.cacheable && this.cacheable();
+    var query = loaderUtils.parseQuery(this.query) || {};
     var tpl = content || '';
-    tpl = cleanRedundantCode(tpl);// remove redundant code
+    tpl = cleanRedundantCode(tpl, query);// remove redundant code
     this.value = tpl;
     return "module.exports = " + JSON.stringify(tpl);
 };
+function def(v, d){
+    return v === null || v === undefined ? d : v;
+}
 // pure regEx to resolve it, not via a parser;
-function cleanRedundantCode(str){
-    if(typeof str === 'string'){
+function cleanRedundantCode(str, opts){
+    opts = opts || {};
+    var minimize = def(opts.minimize, true);
+    var comments = opts.comments || {};
+    var htmlComments = comments.html,
+        rglComments = comments.rgl;
+
+    if(minimize && typeof str === 'string'){
         var SINGLE_SPACE = ' ';
         var EMPTY = '';
+
         // remove html-comments <!-- xxx -->
-        str = str.replace(/<!-[\s\S]*?-->/g, EMPTY);
+        str = !htmlComments ? str.replace(/<!-[\s\S]*?-->/g, EMPTY) : str;
+
         // remove regular-comments {! xxx !}
-        str = str.replace(/{![\s\S]*?!}/g, EMPTY);
+        str = !rglComments ? str.replace(/{![\s\S]*?!}/g, EMPTY) : str;
 
         // 暴力全局替换\s，副作用：内容里面有带空格或回车的字符串会被替换截掉
         str = str.replace(/[\s]{2,}/g, SINGLE_SPACE);
@@ -40,7 +54,6 @@ function cleanRedundantCode(str){
         // str = str.replace(/(>|<)(\s+)/g, function($0, $1){  // 右空格
         //     return $1 + SINGLE_SPACE;
         // });
-
 
         // Last, trim
         str = str.trim();
